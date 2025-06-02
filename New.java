df72fe4eb64e418adb09ea70064b89114e8f301c
Selva73582 <nobiselva73582@gmail.com>
2025-06-02 16:41:04 +0530

    i am from login-page

--- New.java
+++ New.java
@@ -3,6 +3,6 @@ public class New {
     public static void main(String[] args)
     {
         System.out.println("bjjdj");
-        System.out.println("merge this in main");
+        System.out.println("i am new merge conflict, in branch login-page");
     }
 }
